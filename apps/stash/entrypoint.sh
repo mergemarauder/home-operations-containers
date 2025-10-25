f8da9f890096207f7169319e15230424461384ac
MergeMarauder <paul2006@gmail.com>
2025-10-25 00:00:10 +0000

    feat(stash): add python requirements install

--- apps/stash/entrypoint.sh
+++ apps/stash/entrypoint.sh
@@ -2,4 +2,17 @@
 
 export USER="stash"
 
+dirs=(
+  /config/plugins
+  /config/scrapers
+)
+
+for dir in "${dirs[@]}"; do
+  # Find all requirements.txt files recursively
+  find "$dir" -type f -name "requirements.txt" | while read -r reqfile; do
+    echo "Installing Python requirements from: $reqfile"
+    pip install --no-cache-dir -r "$reqfile"
+  done
+done
+
 exec /app/stash "@"
